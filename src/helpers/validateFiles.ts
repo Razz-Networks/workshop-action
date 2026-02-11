747dc3d4ee31e0aacad65b62b19072a37bc6eeed
Erik <33792954+exliipso@users.noreply.github.com>
2026-02-11 03:04:28 -0600

    Remove lowercase validation for file paths
    Removed the validation check for lowercase file paths.

--- src/helpers/validateFiles.ts
+++ src/helpers/validateFiles.ts
@@ -1,7 +1,3 @@
 export default function validateFiles (filePaths: Array<string>) {
-	for (const filePath of filePaths) {
-		if (filePath !== filePath.toLowerCase()) {
-			throw new Error (`${filePath} is not lower case! Please rectify this.`);
-		}
-	}
+	void filePaths;
 }
