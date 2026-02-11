6e1a06d483f7db233b59243c1458b933a555de47
Erik <33792954+exliipso@users.noreply.github.com>
2026-02-11 03:04:11 -0600

    Remove lowercase validation from validateFiles function
    Removed validation for lowercase file paths in validateFiles function.

--- dist/helpers/validateFiles.js
+++ dist/helpers/validateFiles.js
@@ -1,10 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function validateFiles(filePaths) {
-    for (const filePath of filePaths) {
-        if (filePath !== filePath.toLowerCase()) {
-            throw new Error(`${filePath} is not lower case! Please rectify this.`);
-        }
-    }
+    void filePaths;
 }
 exports.default = validateFiles;
